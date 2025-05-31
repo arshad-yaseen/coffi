f46878a58fe9bc85e009c5b25fddb7d870e08fa7
arshad <arshadpyaseen@gmail.com>
2025-05-31 20:35:03 +0530

    chore: add exports field

--- bunup.config.ts
+++ bunup.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "bunup";
-import { report } from "bunup/plugins";
+import { exports } from "bunup/plugins";
 
 export default defineConfig({
     entry: ["src/index.ts"],
     format: ["esm", "cjs"],
     dts: true,
     minify: true,
-    plugins: [report()],
+    plugins: [exports()],
 });
